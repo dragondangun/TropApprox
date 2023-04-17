fcb2d4e2a96d1b261ff6c35953a89b73c0387fba
Шкурат Данил Евгеньевич <shkurat.d@spbec-mining.ru>
2023-04-17 15:02:02 +0300

    Tests for get tropical identity matrix

---
+++ TropApproxTests/GetIdentityMatrixTests.cs
@@ -0,0 +1,39 @@
+﻿using AngouriMath;
+using TropApprox;
+
+namespace TropApproxTests {
+    [TestClass]
+    public class GetIdentityMatrixTests {
+        [TestMethod]
+        public void GetIdentityMatrix_n_3() {
+            // Arenge 
+            int n = 3;
+
+            var expected = MathS.Matrices.Matrix(n, n,
+                Current.Algebra.One, Current.Algebra.Zero, Current.Algebra.Zero,
+                Current.Algebra.Zero, Current.Algebra.One, Current.Algebra.Zero,
+                Current.Algebra.Zero, Current.Algebra.Zero, Current.Algebra.One);
+
+            // Act
+            var given = TropicalMatrixOperations.GetIdentityMatrix(n);
+
+            // Assert
+            Assert.AreEqual(expected, given);
+        }
+
+        [TestMethod]
+        public void GetIdentityMatrix_n_1() {
+            // Arenge 
+            int n = 1;
+
+            var expected = MathS.Matrices.Matrix(n, n,
+                Current.Algebra.One);
+
+            // Act
+            var given = TropicalMatrixOperations.GetIdentityMatrix(n);
+
+            // Assert
+            Assert.AreEqual(expected, given);
+        }
+    }
+}
