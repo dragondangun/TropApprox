421481befabdeb383cabbe0dc158945b9ca2de67
Шкурат Данил Евгеньевич <shkurat.d@spbec-mining.ru>
2023-04-17 15:08:48 +0300

    Tests for Kleene Star

---
+++ TropApproxTests/KleeneStarTests.cs
@@ -0,0 +1,28 @@
+﻿using AngouriMath;
+using TropApprox;
+
+
+namespace TropApproxTests {
+    [TestClass]
+    public class KleeneStarTests {
+        [TestMethod]
+        public void KleeneStar_nInf_1_1_nr_nInf_nInf_1_nr_n2_n1_nInf() {
+            // Arenge 
+            var A = MathS.Matrices.Matrix(3, 3,
+                Current.Algebra.Zero, 1, 1,
+                Current.Algebra.Zero, Current.Algebra.Zero, 1,
+                -2, -1, Current.Algebra.Zero);
+
+            var expected = MathS.Matrices.Matrix(3, 3,
+                0, 1, 2,
+                -1, 0, 1,
+                -2, -1, 0);
+
+            // Act
+            var given = TropicalMatrixOperations.KleeneStar(A);
+
+            // Assert
+            Assert.AreEqual(expected, given);
+        }
+    }
+}
