e5086a48cfc38de4e45856f812ff492a3e247733
dragondangun <dan.sura@mail.ru>
2023-04-26 12:10:14 +0300

    New test for tropical matrix scalar multiplication

--- TropApproxTests/TropicalMatrixScalarMultiplicationTests.cs
+++ TropApproxTests/TropicalMatrixScalarMultiplicationTests.cs
@@ -118,5 +118,77 @@ namespace TropApproxTests {
 
             Assert.IsTrue(result);
         }
+
+        [TestMethod]
+        public void TropicalMatrixScalarMultiplication_A_n1_n2_zero_nr_5_2_n1_nr_3_0_1_B_n3() {
+            using var _ = Settings.DowncastingEnabled.Set(false);
+
+            var A = MathS.Matrices.Matrix(3, 3,
+                2, 1, Current.Algebra.Zero,
+                5, 2, -1,
+                3, 0, 1);
+
+            Number.Real B = -3;
+
+            var expected = MathS.Matrices.Matrix(3, 3,
+                -1, -2, Current.Algebra.Zero,
+                2, -1, -4,
+                0, -3, -2);
+
+            // Act
+            var given = TropicalMatrixOperations.TropicalMatrixScalarMultiplication(A, B);
+
+            // Assert
+            bool result = true;
+            for(int i = 0; i < given.RowCount; i++) {
+                for(int j = 0; j < given.ColumnCount; j++) {
+                    var r = (Number.Real)expected[i, j];
+                    var l = (Number.Real)given[i, j];
+                    if(r > l || l > r) {
+                        result = false;
+                        break;
+                    }
+                }
+            }
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void TropicalMatrixScalarMultiplication_A_n1_n2_zero_nr_5_2_n1_nr_3_0_1_B_n3r() {
+            using var _ = Settings.DowncastingEnabled.Set(false);
+
+            var A = MathS.Matrices.Matrix(3, 3,
+                2, 1, Current.Algebra.Zero,
+                5, 2, -1,
+                3, 0, 1);
+
+            var SpectralRadius = TropicalMatrixOperations.GetSpectralRadius(A);
+
+            var SpectralRadiusInversed = (Number.Real)Current.Algebra.Calculate($"({SpectralRadius})^(-1)");
+
+            var expected = MathS.Matrices.Matrix(3, 3,
+                -1, -2, Current.Algebra.Zero,
+                2, -1, -4,
+                0, -3, -2);
+
+            // Act
+            var given = TropicalMatrixOperations.TropicalMatrixScalarMultiplication(A, SpectralRadiusInversed);
+
+            // Assert
+            bool result = true;
+            for(int i = 0; i < given.RowCount; i++) {
+                for(int j = 0; j < given.ColumnCount; j++) {
+                    var r = (Number.Real)expected[i, j];
+                    var l = (Number.Real)given[i, j];
+                    if(r > l || l > r) {
+                        result = false;
+                        break;
+                    }
+                }
+            }
+
+            Assert.IsTrue(result);
+        }
     }
 }
