db40a422c184cfe8cc382b29d4719dc2d59fb17a
dragondangun <dan.sura@mail.ru>
2023-05-09 18:14:07 +0300

    Disable downcasting

--- TropApproxTests/AreMatriciesEqualTests.cs
+++ TropApproxTests/AreMatriciesEqualTests.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static AngouriMath.MathS;
 
 namespace TropApprox {
     [TestClass]
@@ -11,6 +12,7 @@ namespace TropApprox {
         [TestMethod]
         public void AreMatriciesEqual_A_1_2_3_nr_1_2_3_nr_1_2_3_B_1_2_3_nr_1_2_3_nr_1_2_3_T() {
             // Arenge
+            using var _ = Settings.DowncastingEnabled.Set(false);
             Entity.Matrix A = MathS.Matrices.Matrix(3, 3,
                 1, 2, 3,
                 1, 2, 3,
@@ -31,6 +33,7 @@ namespace TropApprox {
         [TestMethod]
         public void AreMatriciesEqual_A_1_2_3_nr_1_2_3_nr_1_2_3_B_1_2_3_nr_1_2_3_nr_1_2_3_nr_1_2_3_F() {
             // Arenge
+            using var _ = Settings.DowncastingEnabled.Set(false);
             Entity.Matrix A = MathS.Matrices.Matrix(3, 3,
                 1, 2, 3,
                 1, 2, 3,
@@ -52,6 +55,7 @@ namespace TropApprox {
         [TestMethod]
         public void AreMatriciesEqual_A_1_2_3_nr_1_2_3_nr_1_2_3_nr_1_2_3_B_1_2_3_nr_1_2_3_nr_1_2_3_F() {
             // Arenge
+            using var _ = Settings.DowncastingEnabled.Set(false);
             Entity.Matrix A = MathS.Matrices.Matrix(4, 3,
                 1, 2, 3,
                 1, 2, 3,
@@ -73,6 +77,7 @@ namespace TropApprox {
         [TestMethod]
         public void AreMatriciesEqual_A_1_2_3_nr_1_2_3_nr_1_2_3_B_1_2_3_4_nr_1_2_3_4_nr_1_2_3_4_F() {
             // Arenge
+            using var _ = Settings.DowncastingEnabled.Set(false);
             Entity.Matrix A = MathS.Matrices.Matrix(3, 3,
                 1, 2, 3,
                 1, 2, 3,
@@ -94,6 +99,7 @@ namespace TropApprox {
         [TestMethod]
         public void AreMatriciesEqual_A_1_2_3_4_nr_1_2_3_4_nr_1_2_3_4_B_1_2_3_nr_1_2_3_nr_1_2_3_F() {
             // Arenge
+            using var _ = Settings.DowncastingEnabled.Set(false);
             Entity.Matrix A = MathS.Matrices.Matrix(3, 4,
                 1, 2, 3, 4,
                 1, 2, 3, 4,
@@ -115,6 +121,7 @@ namespace TropApprox {
         [TestMethod]
         public void AreMatriciesEqual_A_1d1_2d1_3d1_nr_n1_n2_n3_nr_1_2_3_B_1d1_2d1_3d1_nr_n1_n2_n3_nr_1_2_3_T() {
             // Arenge
+            using var _ = Settings.DowncastingEnabled.Set(false);
             Entity.Matrix A = MathS.Matrices.Matrix(3, 3,
                 1.1, 2.1, 3.1,
                 -1, -2, -3,
@@ -135,6 +142,7 @@ namespace TropApprox {
         [TestMethod]
         public void AreMatriciesEqual_A_1d1_2d1_3d1_nr_n1_n2_n3_nr_1_2_3_B_1d1_2d1_3d1_nr_n1_2_n3_nr_1_2_3_F() {
             // Arenge
+            using var _ = Settings.DowncastingEnabled.Set(false);
             Entity.Matrix A = MathS.Matrices.Matrix(3, 3,
                 1.1, 2.1, 3.1,
                 -1, -2, -3,
@@ -155,6 +163,7 @@ namespace TropApprox {
         [TestMethod]
         public void AreMatriciesEqual_A_1_nr_2_nr_3d1_B_1_nr_2_nr_3d1_T() {
             // Arenge
+            using var _ = Settings.DowncastingEnabled.Set(false);
             Entity.Matrix A = MathS.Vector(1, 2, 3.1);
 
             Entity.Matrix B = MathS.Vector(1, 2, 3.1);
