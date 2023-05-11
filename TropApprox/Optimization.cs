200386f9ab6764670e3144643d1ee692e9eccb02
dragondangun <dan.sura@mail.ru>
2023-05-11 12:26:03 +0300

    Solve two-sided equation

---
+++ TropApprox/Optimization.cs
@@ -0,0 +1,194 @@
+﻿using AngouriMath;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static AngouriMath.Entity;
+using TMO = TropApprox.TropicalMatrixOperations;
+
+namespace TropApprox {
+    public static class Optimization {
+
+        public static void SolveTwoSidedEquation
+        (
+            Entity.Matrix A, Entity.Matrix B,
+            out Entity.Matrix theta, out Entity.Matrix sigma,
+            out Number.Real Delta,
+            Algebra algebra,
+            Entity.Matrix? x_0 = null 
+        ) 
+        {
+            int i = 0;
+
+            List<Entity.Matrix> x = new() {
+                x_0 ?? TMO.GetVectorOfOnes(A.RowCount, algebra),
+            };
+
+            List<Entity.Matrix> y = new();
+
+            Number.Real sqrtDelta;
+
+            while(true) {
+                var Ax_i = TMO.TropicalMatrixMultiplication(A, x[i], algebra);
+                var Ax_iPIed = TMO.PseudoInverse(Ax_i, algebra);
+                var Ax_iPIedB = TMO.TropicalMatrixMultiplication(Ax_iPIed, B, algebra);
+                var important = TMO.PseudoInverse(Ax_iPIedB, algebra);
+                var BImportant = TMO.TropicalMatrixMultiplication(B, important, algebra);
+                var BImportantPIed = TMO.PseudoInverse(BImportant, algebra);
+                var BImportantPIedA = TMO.TropicalMatrixMultiplication(BImportantPIed, A, algebra);
+                Delta = (Number.Real)TMO.TropicalMatrixMultiplication(BImportantPIedA, x[i], algebra)[0, 0];
+                sqrtDelta = (Number.Real)algebra.Calculate($"{Delta}^(1/2)");
+
+                var y_iplus1 = TMO.TropicalMatrixScalarMultiplication(important, sqrtDelta, algebra);
+                y.Add(y_iplus1);
+
+                Trace.WriteLine($"Delta: {Delta}\n{y_iplus1.ToString(true)}\n\n");
+
+                if(StopCondition(Delta, y_iplus1, x, algebra)) {
+                    break;
+                }
+
+                var By_i = TMO.TropicalMatrixMultiplication(B, y[i], algebra);
+                var By_iPIed = TMO.PseudoInverse(By_i, algebra);
+                var By_iPIedA = TMO.TropicalMatrixMultiplication(By_iPIed, A, algebra);
+                important = TMO.PseudoInverse(By_iPIedA, algebra);
+                var AImportant = TMO.TropicalMatrixMultiplication(A, important, algebra);
+                var AImportantPIed = TMO.PseudoInverse(AImportant, algebra);
+                var AImportantPIedB = TMO.TropicalMatrixMultiplication(AImportantPIed, B, algebra);
+                Delta = (Number.Real)TMO.TropicalMatrixMultiplication(AImportantPIedB, y[i], algebra)[0, 0];
+                sqrtDelta = (Number.Real)algebra.Calculate($"{Delta}^(1/2)");
+
+                var x_iplus1 = TMO.TropicalMatrixScalarMultiplication(important, sqrtDelta, algebra);
+                x.Add(x_iplus1);
+                Trace.WriteLine($"Delta: {Delta}\n{x_iplus1.ToString(true)}\n\n");
+
+                if(StopCondition(Delta, x_iplus1, x, algebra)) {
+                    break;
+                }
+
+                ++i;
+            }
+
+            theta = x[^1];
+            sigma = y[^1];
+        }
+
+        public static void SolveTwoSidedEquation
+        (
+            Entity.Matrix A, Entity.Matrix B,
+            out Entity.Matrix theta, out Entity.Matrix sigma,
+            out Number.Real Delta,
+            Entity.Matrix? x_0 = null
+        )
+        => SolveTwoSidedEquation(A, B, out theta, out sigma, out Delta, Current.Algebra, x_0);
+
+        public static void SolveTwoSidedEquation
+        (
+            Entity.Matrix A, Entity.Matrix B,
+            out Entity.Matrix theta, out Entity.Matrix sigma,
+            Entity.Matrix? x_0 = null
+        )
+        => SolveTwoSidedEquation(A, B, out theta, out sigma, out _, Current.Algebra, x_0);
+
+        public static void SolveTwoSidedEquation
+        (
+            Entity.Matrix A, Entity.Matrix B,
+            out Entity.Matrix theta, out Entity.Matrix sigma,
+            Algebra algebra,
+            Entity.Matrix? x_0 = null
+        )
+        => SolveTwoSidedEquation(A, B, out theta, out sigma, out _, algebra, x_0);
+
+        public static void PipeSolveTwoSidedEquation
+        (
+            Entity.Matrix A, Entity.Matrix B,
+            out Entity.Matrix theta, out Entity.Matrix sigma,
+            out Number.Real Delta,
+            Algebra algebra,
+            Entity.Matrix? x_0 = null
+        ) 
+        {
+            int i = 0;
+
+            List<Entity.Matrix> x = new() {
+                TMO.GetVectorOfOnes(A.RowCount, algebra),
+            };
+
+            List<Entity.Matrix> y = new();
+
+            Number.Real sqrtDelta;
+
+            while(true) {
+                var important = A.TropicalMatrixMultiplication(x[i], algebra).PseudoInverse(algebra).TropicalMatrixMultiplication(B, algebra).PseudoInverse(algebra);
+                Delta = (Number.Real)(B.TropicalMatrixMultiplication(important, algebra).PseudoInverse(algebra)
+                    .TropicalMatrixMultiplication(A, algebra).TropicalMatrixMultiplication(x[i], algebra))[0, 0];
+                sqrtDelta = (Number.Real)algebra.Calculate($"{Delta}^(1/2)");
+
+                var y_iplus1 = TMO.TropicalMatrixScalarMultiplication(important, sqrtDelta, algebra);
+                y.Add(y_iplus1);
+
+                if(StopCondition(Delta, y_iplus1, x, algebra)) {
+                    break;
+                }
+
+                important = B.TropicalMatrixMultiplication(y[i], algebra).PseudoInverse(algebra).TropicalMatrixMultiplication(A, algebra).PseudoInverse(algebra);
+                Delta = (Number.Real)(A.TropicalMatrixMultiplication(important, algebra).PseudoInverse(algebra).
+                    TropicalMatrixMultiplication(B, algebra).TropicalMatrixMultiplication(y[i], algebra))[0, 0];
+                sqrtDelta = (Number.Real)algebra.Calculate($"{Delta}^(1/2)");
+
+                var x_iplus1 = important.TropicalMatrixScalarMultiplication(sqrtDelta, algebra);
+                x.Add(x_iplus1);
+
+                if(StopCondition(Delta, x_iplus1, x, algebra)) {
+                    break;
+                }
+
+                ++i;
+            }
+
+            theta = x[^1];
+            sigma = y[^1];
+        }
+
+        public static void PipeSolveTwoSidedEquation
+        (
+            Entity.Matrix A, Entity.Matrix B,
+            out Entity.Matrix theta, out Entity.Matrix sigma,
+            out Number.Real Delta,
+            Entity.Matrix? x_0 = null
+        )
+        => PipeSolveTwoSidedEquation(A, B, out theta, out sigma, out Delta, Current.Algebra, x_0);
+
+        public static void PipeSolveTwoSidedEquation
+        (
+            Entity.Matrix A, Entity.Matrix B,
+            out Entity.Matrix theta, out Entity.Matrix sigma,
+            Entity.Matrix? x_0 = null
+        )
+        => PipeSolveTwoSidedEquation(A, B, out theta, out sigma, out _, Current.Algebra, x_0);
+
+        public static void PipeSolveTwoSidedEquation
+        (
+            Entity.Matrix A, Entity.Matrix B,
+            out Entity.Matrix theta, out Entity.Matrix sigma,
+            Algebra algebra,
+            Entity.Matrix? x_0 = null
+        )
+        => PipeSolveTwoSidedEquation(A, B, out theta, out sigma, out _, algebra, x_0);
+
+        private static bool StopCondition(Number.Real Delta, Entity.Matrix vector, List<Entity.Matrix> vectorCollection, Algebra algebra) {
+            if(!(Delta > algebra.One || Delta < algebra.One)) {
+                return true;
+            }
+            for(int j = 0; j < vectorCollection.Count - 1; j++) {
+                if(TMO.AreMatriciesEqual(vectorCollection[j], vector)) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ TropApproxTests/SolveTwoSidedEquationTests.cs
@@ -0,0 +1,84 @@
+﻿using AngouriMath;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TropApprox;
+
+namespace TropApproxTests {
+    [TestClass]
+    public class SolveTwoSidedEquationTests {
+        [TestMethod]
+        public void SolveTwoSidedEquationTest_A_3_zero_0_nr_1_1_0_zero_1_2_B_1_1_nr_3_2_nr_3_1_x0_5_3_1() {
+            // Arrange
+            using var _ = MathS.Settings.DowncastingEnabled.Set(false);
+            Current.Algebra = MaxPlus.Instance;
+
+            var A = MathS.Matrices.Matrix(3, 3,
+                3, Current.Algebra.Zero, 0,
+                1, 1, 0,
+                Current.Algebra.Zero, 1, 2
+                );
+
+            var B = MathS.Matrices.Matrix(3, 2,
+                1, 1,
+                3, 2,
+                3, 1
+                );
+
+            var x_0 = MathS.Vector(5, 3, 1);
+
+            //var thetaExpected = MathS.Vector(4, 5, 4);
+            var thetaExpected = MathS.Vector(3, 6, 5);
+            //var sigmaExpected = MathS.Vector(3, 4);
+            var sigmaExpected = MathS.Vector(4, 5);
+
+            // Act
+            Entity.Matrix theta;
+            Entity.Matrix sigma;
+            TropApprox.Optimization.SolveTwoSidedEquation(A, B, out theta, out sigma, x_0);
+
+            // Assert
+            bool isThetaCorrect = TropApprox.TropicalMatrixOperations.AreMatriciesEqual(theta, thetaExpected);
+            bool isSigmaCorrect = TropApprox.TropicalMatrixOperations.AreMatriciesEqual(sigma, sigmaExpected);
+            Assert.IsTrue(isThetaCorrect && isSigmaCorrect);
+        }
+
+        [TestMethod]
+        public void SolveTwoSidedEquationTest_A_3_zero_0_nr_1_1_0_zero_1_3_B_1_1_nr_3_2_nr_1_1_x0_5_3_1() {
+            // Arrange
+            using var _ = MathS.Settings.DowncastingEnabled.Set(false);
+            Current.Algebra = MaxPlus.Instance;
+
+            var A = MathS.Matrices.Matrix(3, 3,
+                3, Current.Algebra.Zero, 0,
+                1, 1, 0,
+                Current.Algebra.Zero, 1, 3
+                );
+
+            var B = MathS.Matrices.Matrix(3, 2,
+                1, 1,
+                3, 2,
+                1, 1
+                );
+
+            var x_0 = MathS.Vector(5, 3, 1);
+
+            //var thetaExpected = MathS.Vector(5, 5, 3);
+            var thetaExpected = MathS.Vector(4, 6, 4);
+            //var sigmaExpected = MathS.Vector(3.5, 4.5);
+            var sigmaExpected = MathS.Vector(4.5, 5.5);
+
+            // Act
+            Entity.Matrix theta;
+            Entity.Matrix sigma;
+            TropApprox.Optimization.SolveTwoSidedEquation(A, B, out theta, out sigma, x_0);
+
+            // Assert
+            bool isThetaCorrect = TropApprox.TropicalMatrixOperations.AreMatriciesEqual(theta, thetaExpected);
+            bool isSigmaCorrect = TropApprox.TropicalMatrixOperations.AreMatriciesEqual(sigma, sigmaExpected);
+            Assert.IsTrue(isThetaCorrect && isSigmaCorrect);
+        }
+    }
+}
